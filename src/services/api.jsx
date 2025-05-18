e546e613b747b141aeca9d4500c53833f1e0c666
Abhinvcodes <abhinavsinghasb@gmail.com>
2025-05-18 18:13:01 +0530

    sixth commit

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
-
+//const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://kmc-dental.onrender.com';
 // Create axios instance with authorization header
 const createAuthHeader = () => {
     const token = localStorage.getItem('token');
